a655c25f35fdd7bb07ac97353c0762e9f50c673a
KarenS <channaru@icloud.com>
2024-05-29 16:53:34 -0700

    Add springboot dependencies and files

---
+++ gradecalc/src/test/java/com/_6/gradecalc/GradecalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com._6.gradecalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradecalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
