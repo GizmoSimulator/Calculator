6bf36cd042ae2c31b90caf18fd3e80bcd7615d2a
KarenS <channaru@icloud.com>
2024-05-29 16:52:03 -0700

    Add springboot dependencies and files

---
+++ gradecalc/src/main/java/com/_6/gradecalc/GradecalcApplication.java
@@ -0,0 +1,13 @@
+package com._6.gradecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradecalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradecalcApplication.class, args);
+	}
+
+}
